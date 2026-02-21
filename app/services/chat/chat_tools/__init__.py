435b9eb2297ed00358d5088050de8ea9b726c668
StableLlama <stablellama@tutanota.com>
2026-02-21 15:12:40 +0100

    Compliance: Add missing copyright notices and apply formatting

--- app/services/chat/chat_tools/__init__.py
+++ app/services/chat/chat_tools/__init__.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2026 StableLlama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 from app.services.chat.chat_tools.chapter_tools import handle_chapter_tool
 from app.services.chat.chat_tools.image_tools import (
     _tool_generate_image_description,
--- app/services/chat/chat_tools/sourcebook_tools.py
+++ app/services/chat/chat_tools/sourcebook_tools.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2026 StableLlama
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 from app.services.chat.chat_tools.common import tool_message
 from app.services.sourcebook.sourcebook_helpers import (
     sb_create,
